8720c3706d48be31e25b409ba360597502221cc1
Afam-Ifediogor, U. Divine <38317208+definite-d@users.noreply.github.com>
2025-11-14 04:21:04 +0100

    chore: add docstring

--- signet/qr.py
+++ signet/qr.py
@@ -9,9 +9,20 @@ from pydantic import validate_call
 
 @validate_call()
 def generate_qr_code(
-    data: str, format: Literal["png", "jpg", "webp", "svg"]
+    data: str, format: Literal["png", "jpg", "webp", "svg"], columns=8
 ) -> BytesIO:
-    qr: list[list[int]] = encode(data)
+    """
+    Generates a QR code from the given data.
+
+    Args:
+        data (str): The data to encode in the QR code.
+        format (Literal["png", "jpg", "webp", "svg"]): The format of the QR code image.
+        columns (int): The number of columns in the QR code.
+
+    Returns:
+        BytesIO: A bytes stream containing the QR code image.
+    """
+    qr: list[list[int]] = encode(data, columns)
     qr_img: BytesIO = BytesIO()
 
     if format == "svg":
